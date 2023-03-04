885670e1bad130de777701c9eccacf4230c2abe6
mr-donor <donor.gm@gmail.com>
2023-03-05 01:20:06 +0800

    refactor: improve load keypair

--- client/ts/src/util/keypair.ts
+++ client/ts/src/util/keypair.ts
@@ -1,31 +1,48 @@
 import { join, resolve } from 'path';
 import fs from 'fs';
-import nacl from 'tweetnacl';
-import { Ed25519Keypair } from '@mysten/sui.js';
+import {
+    Ed25519Keypair,
+    Secp256k1Keypair,
+    fromExportedKeypair,
+} from '@mysten/sui.js';
+import type { SignatureScheme } from '@mysten/sui.js';
 
-export const loadKeypair = async (name: string, createWhenNotExist = false) => {
+export const loadKeypair = async (
+    name: string,
+    schema: SignatureScheme = 'ED25519',
+    createWhenNotExist = false
+) => {
     const keysPath = resolve(__dirname, '../keys');
 
     if (!fs.existsSync(keysPath)) fs.mkdirSync(keysPath);
 
     if (fs.existsSync(join(keysPath, `${name}.json`))) {
-        const secretKey = new Uint8Array(
-            JSON.parse(fs.readFileSync(join(keysPath, `${name}.json`), 'utf-8'))
+        const exportedKeypair = JSON.parse(
+            fs.readFileSync(join(keysPath, `${name}.json`), 'utf-8')
         );
 
-        return Ed25519Keypair.fromSecretKey(secretKey);
+        return fromExportedKeypair(exportedKeypair);
     } else {
-        const keypair = nacl.sign.keyPair();
-
+        let keypair: Ed25519Keypair | Secp256k1Keypair;
+        switch (schema) {
+            case 'ED25519':
+                keypair = Ed25519Keypair.generate();
+                break;
+            case 'Secp256k1':
+                keypair = Secp256k1Keypair.generate();
+            default:
+                throw new Error(`Invalid keypair schema ${schema}`);
+        }
+        const exportKeypair = keypair.export();
         if (createWhenNotExist) {
             fs.writeFileSync(
                 join(keysPath, `${name}.json`),
-                JSON.stringify(Array.from(keypair.secretKey))
+                JSON.stringify(exportKeypair)
             );
         } else {
             throw new Error('keypair is not exist');
         }
 
-        return Ed25519Keypair.fromSecretKey(keypair.secretKey);
+        return fromExportedKeypair(exportKeypair);
     }
 };
